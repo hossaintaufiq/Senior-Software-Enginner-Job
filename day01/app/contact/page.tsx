cd36c9923f581715441f8122c7df91cf8369ca54
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-04 00:54:33 +0600

    day01

---
+++ day01/app/contact/page.tsx
@@ -0,0 +1,11 @@
+
+
+const contact = () => {
+  return (
+    <div>
+      <h2>Hello from Contact Page</h2>
+    </div>
+  )
+}
+
+export default contact
---
+++ day01/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className=" mt-4 flex ">Hello world</h1>
+    </>
+  );
+}
