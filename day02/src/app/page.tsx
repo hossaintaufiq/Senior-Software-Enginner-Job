cd33c251f52b17fa1d5eaca7b3c4510b8a449103
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-04 20:09:50 +0600

    day02

---
+++ day02/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/component/Header"
+
+
+export default function Home() {
+  return (
+    <div className=" min-h-screen items-center justify-center font-sans">
+      <Header/>
+      <h1>this is a home page 
+
+      </h1>
+      
+    </div>
+  );
+}
---
+++ day02/src/component/Header.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">My Application Header</h1>
+    </header>
+  );
+}
