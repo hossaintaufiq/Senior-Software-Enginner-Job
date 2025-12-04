367d72046bb5d79ee7082a949121363199632abb
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-04 20:35:14 +0600

    passing props

--- day02/src/app/page.tsx
+++ day02/src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/component/Header"
 export default function Home() {
   return (
     <div className=" min-h-screen items-center justify-center font-sans">
-      <Header/>
+      <Header name={"Hossain Ahmmed Taufiq"} age={30} profession={"Developer"} />
       <h1>this is a home page 
 
       </h1>
--- day02/src/component/Header.tsx
+++ day02/src/component/Header.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-export default function Header() {
+export default function Header({name, age, profession}: {name: string; age: number; profession: string}) {
+    // const name= "Hossain Ahmmed Taufiq"
   return (
-    <header className="w-full bg-blue-600 text-white p-4">
+    <header className="w-full bg-blue-600 text-white p-4" >
+        {/* {name} */}
+        {name} - {age} - {profession}
       <h1 className="text-2xl font-bold">My Application Header</h1>
     </header>
   );
