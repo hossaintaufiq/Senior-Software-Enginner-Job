d761ab2d8228f59f96c85d9fec00867de268d34b
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-06 19:40:24 +0600

    done day 03

--- day03/src/app/page.tsx
+++ day03/src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+// import Image from "next/image";
+// 'use client';
+type post={
+  userId:number;
+  id:number;
+  title:string;
+  body:string;
+}
 
-export default function Home() {
+export default async function Home() {
+
+  const res= await fetch('http://localhost:3000/api/posts',{cache:'no-store'});
+  const posts:post[]= await res.json(); 
+
+
   return (
     <div >
       <h1 className="flex justify-center items-center p4">This is day-03</h1>
+      {
+        posts.slice(0,5).map((post)=>(
+          <div key={post.id} className="m-4 p-4 border-2 rounded-lg">
+            <h2 className="font-bold text-xl">{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
