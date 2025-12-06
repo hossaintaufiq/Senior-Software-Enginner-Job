fdac185f6c8533ccf5ec31cac03ce52d6053f60a
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-06 18:54:01 +0600

    day03

---
+++ day03/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1 className="flex justify-center items-center p4">This is day-03</h1>
+    </div>
+  );
+}
