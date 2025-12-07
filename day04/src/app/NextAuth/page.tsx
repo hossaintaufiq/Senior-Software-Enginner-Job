d7d80e8888c888f93f45b9d3331ab70b3117f3db
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-07 21:16:17 +0600

    next auth

--- day04/src/app/NextAuth/page.tsx
+++ day04/src/app/NextAuth/page.tsx
@@ -1,7 +1,7 @@
 export default function FirebasePage() {
   return (
     <div>
-      <h1 className="text-center p-2">Firebase Integration Page</h1>
+      <h1 className="text-center p-2">Next auth Integration Page</h1>
       <p className="flex justify-center items-center flex-col py-2">
         This page is dedicated to NextAuth functionalities.
       </p>
