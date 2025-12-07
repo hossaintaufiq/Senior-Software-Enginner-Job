011136fb24a25b2d3bdbb82eb5397d4d3fee6a10
hossaintaufiq <hossainahmmedtaufiq22@gmail.com>
2025-12-07 18:30:36 +0600

    defining and creating a seperate page for every topic

---
+++ day04/src/app/Hooks/page.tsx
@@ -0,0 +1,10 @@
+export default function FirebasePage() {
+  return (
+    <div>
+      <h1 className="text-center p-2">Firebase Integration Page</h1>
+      <p className="flex justify-center items-center flex-col py-2">
+        This page is dedicated to Firebase functionalities.
+      </p>
+    </div>
+  );
+}
