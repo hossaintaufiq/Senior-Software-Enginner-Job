48565a0c21e17315d8bec7ab31f4c000791fc280
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-04-06 15:03:34 +0600

    sending data  to the fronend and geting it

---
+++ Milestone 10/Module_54/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const cors = require('cors');
+const app= express(); 
+const port = process.env.PORT || 3000; 
+
+app.use(cors()); // Enable CORS for all routes
+
+
+const users= [
+    {id:1, name:'Alice', email:'alice@example.com'}, 
+    {id:2, name:'Bob', email:'bob@gmail.com'},
+    {id:3, name:'Charlie', email:'charlie@example.com'}, 
+
+]
+
+app.get('/', (req, res) => {
+    res.send('User Management Server is running');
+    
+});
+
+app.get('/users', (req,res)=>{
+    res.json(users); 
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ Milestone 10/Module_54/frontend/app/welcome/welcome.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  event?: React.FormEvent<HTMLFormElement>;
+}
+
+export function Welcome() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    const formData = event.target; 
+    const name= formData.name.value; 
+    const email= formData.email.value; 
+    const userData = { name, email };
+
+    console.log(userData)
+
+  }
+
+  return (
+    <>
+      users Authentication System
+      <h3>Numbers of Users: {users.length}</h3>
+
+      <h1>displying form </h1>
+
+      <form onSubmit={handleFormSubmit}>
+        Name:
+        <input type="text" name="name" id="" placeholder="Name" className="border-2 border-gray-300 rounded-md p-2" />
+        <br />
+        Email:
+        <input type="email" name="email" id="" placeholder="Email" className="border-2 border-gray-300 rounded-md p-2" />
+        <br />
+        <button type="submit">submit User</button>
+      </form>
+
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <p>User Information</p>
+            <p>ID: {user.id} - Name: {user.name} - Email: {user.email}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+
