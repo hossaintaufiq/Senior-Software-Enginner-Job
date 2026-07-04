a64cde14eee9fb2e06e1ac9c58422a97c6254002
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-07-04 19:35:37 +0600

    fixing the possible bugs in typescripts

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,42 +1,42 @@
- const menu= [
-    {name: "Margarita", price: 8}, 
-    {name: "Peppeerpni", price:10}, 
-    {name: "Hawaiian", price: 10}, 
-    {name: "Veggie", price: 9}, 
- ]
+//  const menu= [
+//     {name: "Margarita", price: 8}, 
+//     {name: "Peppeerpni", price:10}, 
+//     {name: "Hawaiian", price: 10}, 
+//     {name: "Veggie", price: 9}, 
+//  ]
 
 
- const cashInRegister= 100
- const nextOrderId= 1 
-const OrderQueue= []
+//  const cashInRegister= 100
+//  const nextOrderId= 1 
+// const OrderQueue= []
 
 
-function addNewPizza(pizzaobj){
-    menu.push(pizzaobj)
-}
+// function addNewPizza(pizzaobj){
+//     menu.push(pizzaobj)
+// }
 
 
-function placeOrder(pizzaName){
-    const selectedPizza= menu.find(pizzaobj=> pizzaobj.name=== pizzaName)
-    cashInRegister+=selectedPizza.price
-    const newOrder={id:nextOrderId++, pizza:selectedPizza, status: "ordered"}
-    OrderQueue.push(newOrder)
-    return newOrder
-}
+// function placeOrder(pizzaName){
+//     const selectedPizza= menu.find(pizzaobj=> pizzaobj.name=== pizzaName)
+//     cashInRegister+=selectedPizza.price
+//     const newOrder={id:nextOrderId++, pizza:selectedPizza, status: "ordered"}
+//     OrderQueue.push(newOrder)
+//     return newOrder
+// }
 
-function completeOrder(orderId){
-    const completedOrder= OrderQueue.find(order=>order.id===orderId)
-    order.status= "Completed"
-    return order
-}
+// function completeOrder(orderId){
+//     const completedOrder= OrderQueue.find(order=>order.id===orderId)
+//     order.status= "Completed"
+//     return order
+// }
 
-addNewPizza({name:"Chiken Bacon Ranch", price: 12})
-addNewPizza({name:"BBQ Chiken", price : 12})
-addNewPizza({name:"Spicy Sausage", price : 11})
+// addNewPizza({name:"Chiken Bacon Ranch", price: 12})
+// addNewPizza({name:"BBQ Chiken", price : 12})
+// addNewPizza({name:"Spicy Sausage", price : 11})
 
-placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
+// placeOrder("Chicken Bacon Ranch")
+// completeOrder("1")
 
-console.log("Menu: ", menu)
-console.log("Cash in Register : ", cashInRegister)
-console.log("Order Queue:  ", OrderQueue)
+// console.log("Menu: ", menu)
+// console.log("Cash in Register : ", cashInRegister)
+// console.log("Order Queue:  ", OrderQueue)
---
+++ TypeScript/index.ts
@@ -0,0 +1,46 @@
+ const menu= [
+    {name: "Margarita", price: 8}, 
+    {name: "Pepperpni", price:10}, 
+    {name: "Hawaiian", price: 10}, 
+    {name: "Veggie", price: 9}, 
+ ]
+
+
+let cashInRegister= 100
+let nextOrderId= 1 
+const OrderQueue= []
+
+
+function addNewPizza(pizzaobj){
+    menu.push(pizzaobj)
+}
+
+
+function placeOrder(pizzaName){
+    const selectedPizza= menu.find(pizzaobj=> pizzaobj.name=== pizzaName)
+    if (!selectedPizza){
+        console.error(`$(pizzaName) does not exist in the menu`)
+        return
+    }
+    cashInRegister+=selectedPizza.price
+    const newOrder={id:nextOrderId++, pizza:selectedPizza, status: "ordered"}
+    OrderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId){
+    const completedOrder= OrderQueue.find(order=>order.id===orderId)
+    order.status= "Completed"
+    return order
+}
+
+addNewPizza({name:"Chiken Bacon Ranch", price: 12})
+addNewPizza({name:"BBQ Chiken", price : 12})
+addNewPizza({name:"Spicy Sausage", price : 11})
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu: ", menu)
+console.log("Cash in Register : ", cashInRegister)
+console.log("Order Queue:  ", OrderQueue)
