0d61ffd7cbba8ca7dbb3b92f327e791d4a23f297
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-07-06 23:48:00 +0600

    search product by id

--- Backend_Game/src/controllers/product.controller.ts
+++ Backend_Game/src/controllers/product.controller.ts
@@ -10,3 +10,19 @@ export const getAllProducts = async ( req:Request, res:Response)=>{
         data:products
     })
 }
+
+export const getProductById = async (req:Request, res:Response)=>{
+    const id= Number(req.params.id) //type casting 
+    const product= await productServices.getProductById(id)
+
+    if(!product){
+        return res.status(404).json({
+            success:false, 
+            message: "Product not found",
+        })
+    }
+    return res.status(200).json({
+        success:true, 
+        data:product, 
+    })
+}
--- Backend_Game/src/services/product.service.ts
+++ Backend_Game/src/services/product.service.ts
@@ -5,3 +5,7 @@ import { Product } from '../interfaces/product.types'
 export const getAllProducts = async():Promise<Product[]> =>{
     return productsData.products
 }
+
+export const getProductById = async(id:number):Promise<Product|undefined> =>{
+    return productsData.products.find((product)=>product.id===id)
+}
