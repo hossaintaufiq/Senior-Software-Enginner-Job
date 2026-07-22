a852e91dc338616bf96e746a916fa66288c00d8d
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-07-22 13:03:03 +0600

    crud 2 done . nothing especial

--- CRUD V2/src/routes/calculator.ts
+++ CRUD V2/src/routes/calculator.ts
@@ -8,13 +8,23 @@ export const calculatorRouter = Router();
 calculatorRouter.get('/calculator', (req:Request,res:Response)=>{
     res.status(200).json({
         success: true, 
-        message: "Server is running"
+        message: "Server is running", 
+        data: [{id: 1, result:1},
+            {id:2, result: 2}, 
+        ]
     })
 })
 
 calculatorRouter.get('/calculator/:id', (req:Request, res:Response)=>{
-    console.log(`Query parameter : ${JSON.stringify(req.query)}`)
-    console.log(`Headers : ${JSON.stringify(req.headers)}`)
-    console.log(`Method: ${req.method}`)
-    res.status(202).header('x-custom-header').send(`Hello ${req.params.id}`)
+    // console.log(`Query parameter : ${JSON.stringify(req.query)}`)
+    // console.log(`Headers : ${JSON.stringify(req.headers)}`)
+    // console.log(`Method: ${req.method}`)
+    res.status(202).header('x-custom-header').send(
+        {message: `Get calculator by id `,
+          id: req.params.id, 
+          result: 1,  
+            
+        }, 
+
+    )
 })
